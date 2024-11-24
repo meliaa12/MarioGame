fcd37f8e3a3ff356ceb065eb7db9651dbf8e91fd
Meryem Jarrar <meryem.jarrar@epitech.eu>
2024-11-24 01:24:33 +0100

    score et annimation

---
+++ core/src/main/java/com/mathmaurer/affichage/Score.java
@@ -0,0 +1,52 @@
+// Score.java
+package com.mathmaurer.affichage;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Score {
+    private int score;
+    private BitmapFont font;
+    private final int POSITION_X = 20;
+    private final int POSITION_Y;
+
+    public Score() {
+        this.score = 0;
+        font = new BitmapFont();
+        font.setColor(Color.WHITE);
+        font.getData().setScale(2.0f);
+        // Positionner le score en haut de l'écran
+        POSITION_Y = Gdx.graphics.getHeight() - 20;
+    }
+
+    public void render(SpriteBatch batch) {
+        // Dessiner le score avec un outline noir pour meilleure visibilité
+        font.setColor(Color.BLACK);
+        for(int xOffset=-1; xOffset<=1; xOffset++) {
+            for(int yOffset=-1; yOffset<=1; yOffset++) {
+                if(xOffset != 0 || yOffset != 0) {
+                    font.draw(batch, "SCORE: " + score, POSITION_X + xOffset, POSITION_Y + yOffset);
+                }
+            }
+        }
+        font.setColor(Color.WHITE);
+        font.draw(batch, "SCORE: " + score, POSITION_X, POSITION_Y);
+    }
+
+    public void addScore(int points) {
+        this.score += points;
+        System.out.println("Score mis à jour: " + score); // Debug
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void dispose() {
+        if (font != null) {
+            font.dispose();
+        }
+    }
+}
--- core/src/main/java/com/mathmaurer/objets/Piece.java
+++ core/src/main/java/com/mathmaurer/objets/Piece.java
@@ -1,72 +1,56 @@
+// Piece.java
 package com.mathmaurer.objets;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Piece extends Objet implements Runnable {
+    private float baseY; // Position Y de base
+    private float currentY; // Position Y actuelle
+    private float amplitude = 10; // Amplitude de l'oscillation
+    private float speed = 0.1f; // Vitesse de l'oscillation
+    private float time = 0; // Temps pour l'animation
+    private boolean isAnimating = true;
 
-    // **** VARIABLES **** //
-    private int compteur;
-    private final int PAUSE = 15; // Temps d'attente entre 2 tours de boucle
-    private Texture texture2;
+    private Texture icoObjet; // Déclaration de l'attribut
 
-    // **** CONSTRUCTEUR **** //
-    public Piece(float x, float y) {
-        super(x, y, 30, 30, "images/piece1.png"); // Appel au constructeur de Objet
-        this.texture2 = new Texture("images/piece2.png"); // Deuxième texture pour l'animation
+
+    public Piece(int x, int y) {
+        super(x, y, 25, 25, "images/piece1.png"); // Ajout du chemin de la texture
+        this.baseY = y;
+        this.currentY = y;
+        this.icoObjet = new Texture("images/piece2.png");
     }
+    
 
-    // **** METHODES **** //
-    @Override
     public void render(SpriteBatch batch) {
-        // Appeler bouge pour changer de texture
-        this.bouge();
-        // Dessiner l'objet avec la texture actuelle
-        batch.draw(this.getTextureObjet(), this.getX(), this.getY(), this.getLargeur(), this.getHauteur());
-    }
-
-    public void bouge() {
-        // Animation : alterner entre deux textures
-        this.compteur++;
-        if (this.compteur / 100 == 0) {
-            this.setTexture(this.texture2); // Changer vers texture2
-        } else {
-            this.setTexture(super.getTextureObjet()); // Revenir à la texture par défaut
-        }
-        if (this.compteur == 200) {
-            this.compteur = 0; // Réinitialiser le compteur
+        if (isAnimating) {
+            // Calculer le mouvement vertical
+            time += speed;
+            currentY = baseY + (float)(Math.sin(time) * amplitude);
+            // Dessiner la pièce à sa position actuelle
+            batch.draw(this.getTextureObjet(), this.getX(), currentY);
         }
     }
 
     @Override
     public void run() {
-        try {
-            Thread.sleep(20); // Attendre 20 ms avant d'exécuter l'animation
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        while (true) {
-            this.bouge();
+        while (isAnimating) {
             try {
-                Thread.sleep(PAUSE);
+                Thread.sleep(16); // ~60 FPS
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
 
+    public void stopAnimation() {
+        isAnimating = false;
+    }
+
     @Override
     public void dispose() {
-        super.dispose(); // Libérer la texture parent
-        if (this.texture2 != null) {
-            this.texture2.dispose(); // Libérer la deuxième texture
-        }
-    }
-
-    // Setter pour changer la texture courante
-    private void setTexture(Texture texture) {
-        this.textureObjet = texture;
+        stopAnimation();
+        super.dispose();
     }
 }
-
